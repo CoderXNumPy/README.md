be1881f8553751d04a8b834661b184106e4f665f
CoderXNumPy <theflowblock3@gmail.com>
2026-09-23 12:43:45 +0530

    Update player.rs

--- player.rs
+++ player.rs
@@ -13,13 +13,17 @@ impl Player {
     }
 
     fn show_status(&self) {
-        println!("{} ki HP: {}", self.name, self.hp);
+        println!("Name {} Hp {}", self.name, self.hp);
+    }
+
+    fn is_alive(&self) -> bool {
+        self.hp > 0
     }
 }
 
 fn main() {
     let mut p1 = Player {
-        name: String::from("Xylon"),
+        name: String::from("XyLon"),
         hp: 100,
     };
 
@@ -28,4 +32,10 @@ fn main() {
 
     p1.damage_player(100);
     p1.show_status();
+
+    if p1.is_alive() {
+        println!("Yes Zinda hai {}", p1.name);
+    } else {
+        println!("Mar gya noobda 🤣");
+    }
 }
